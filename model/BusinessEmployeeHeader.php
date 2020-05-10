e6935f1cdadb58275ad390e3cc4568f6f3ddcb97
sabietusho7 <64490188+sabietusho7@users.noreply.github.com>
2020-05-10 23:47:34 +0200

    Add files via upload

---
+++ model/BusinessEmployeeHeader.php
@@ -0,0 +1,86 @@
+<?php
+
+class BusinessEmployeeHeader
+{
+    private $id;
+    private $businessId;
+    private $employeeId;
+    private $freeEmployee;
+
+
+
+    /**
+     * businessHeader constructor.
+     * @param $id
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBusinessId()
+    {
+        return $this->businessId;
+    }
+
+    /**
+     * @param mixed $businessId
+     */
+    public function setBusinessId($businessId)
+    {
+        $this->businessId = $businessId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmployeeId()
+    {
+        return $this->employeeId;
+    }
+
+    /**
+     * @param mixed $employeeId
+     */
+    public function setEmployeeId($employeeId)
+    {
+        $this->employeeId = $employeeId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFreeEmployee()
+    {
+        return $this->freeEmployee;
+    }
+
+    /**
+     * @param mixed $freeEmployee
+     */
+    public function setFreeEmployee($freeEmployee): void
+    {
+        $this->freeEmployee = $freeEmployee;
+    }
+
+
+}
