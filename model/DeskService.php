1eaddf55e2098691c0183e85bf1c16822906e358
sabietusho7 <64490188+sabietusho7@users.noreply.github.com>
2020-05-10 23:48:47 +0200

    Add files via upload

---
+++ model/DeskService.php
@@ -0,0 +1,155 @@
+<?php
+
+
+class DeskService
+{
+  private $id;
+  private $name;
+  private $businessId;
+  private $etc;
+  private $imgUrl;
+  private $sportelist;
+  private $counter;
+  private $isActive;
+  public static $MAX_ETC=120;
+
+    /**
+     * @return mixed
+     */
+    public function getisActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCounter()
+    {
+        return $this->counter;
+    }
+
+    /**
+     * @param mixed $counter
+     */
+    public function setCounter($counter)
+    {
+        $this->counter = $counter;
+    }
+
+    /**
+     * deskService constructor.
+     * @param $id
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBusinessId()
+    {
+        return $this->businessId;
+    }
+
+    /**
+     * @param mixed $businessId
+     */
+    public function setBusinessId($businessId)
+    {
+        $this->businessId = $businessId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEtc()
+    {
+        return $this->etc;
+    }
+
+    /**
+     * @param mixed $etc
+     */
+    public function setEtc($etc)
+    {
+        $this->etc = $etc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImgUrl()
+    {
+        return $this->imgUrl;
+    }
+
+    /**
+     * @param mixed $imgUrl
+     */
+    public function setImgUrl($imgUrl)
+    {
+        $this->imgUrl = $imgUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSportelist()
+    {
+        return $this->sportelist;
+    }
+
+    /**
+     * @param mixed $sportelist
+     */
+    public function setSportelist($sportelist)
+    {
+        $this->sportelist = $sportelist;
+    }
+
+
+
+}
