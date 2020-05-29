a961d2391a4525f8d8bf15c83c74bbda25e4b0ac
sabietusho7 <64490188+sabietusho7@users.noreply.github.com>
2020-05-29 22:36:24 +0200

    Update BusinessController.php

--- controller/BusinessController.php
+++ controller/BusinessController.php
@@ -34,16 +34,7 @@ class BusinessController
        return $deskServices[0];
    }
    
-      public function getDeskServicesByBusinessId($businessID)
-    {
-        if($businessID == null) {
-            return array();
-        }
-        $filter = array("ID" => null, "BusinessID" => $businessID,"Name"=>null, "SportelistID" => null, "isActive" => null);
-        $businessDao = new BusinessDAO();
-        $deskServices = $businessDao->getDeskService($filter);
-        return $deskServices;
-    }
+     
 	
     public function getBusinessById($id)
     {
@@ -62,6 +53,7 @@ class BusinessController
         $business = $arr[0];
         return $business;
     }
+    
 
     public function getActiveBusinesses()
     {
@@ -86,18 +78,83 @@ class BusinessController
          $businessEmployeeHeader = $businessEmployeeHeaders[0];
          return $businessEmployeeHeader->getBusinessId();
     }
-
-    public function getActiveDeskServiceByBusinessId($id)
-    {
-        if($id == null) {
+    public function getEmployeesByBusinessId($businessId) {
+        if($businessId == null) {
             return array();
         }
-        $filter = array("ID" => null, "BusinessID" => $id, "Name" => null,"SportelistID" => null, "isActive" => 1);
         $businessDao = new BusinessDAO();
-        return $businessDao->getDeskService($filter);
-
+        $filter = array("BusinessID" => $businessId, "EmployeeID" => null, "Free"=>null, "IsActive"=>null, "NoManager"=>true);
+        return $businessDao->getBusinessEmployeeHeader($filter);
     }
+
+        public function getActiveDeskServiceByID($id)
+        {
+            $filter = array("ID" => $id, "BusinessID" => null, "Name" => null,"SportelistID" => null, "isActive" => 1);
+            $businessDao = new BusinessDAO();
+            $arr = $businessDao->getDeskService($filter);
+            return $arr;
+        }
     
+        public function getDeskServicesByBusinessId($businessID)
+        {
+            if($businessID == null) {
+                return array();
+            }
+            $filter = array("ID" => null, "BusinessID" => $businessID,"Name"=>null, "SportelistID" => null, "isActive" => null);
+            $businessDao = new BusinessDAO();
+            $deskServices = $businessDao->getDeskService($filter);
+            return $deskServices;
+        }
+        public function getActiveDeskServiceByBusinessId($id)
+        {
+            if($id == null) {
+                return array();
+            }
+            $filter = array("ID" => null, "BusinessID" => $id, "Name" => null,"SportelistID" => null, "isActive" => 1);
+            $businessDao = new BusinessDAO();
+            return $businessDao->getDeskService($filter);
+    
+        }
+        public function getDeskServiceCounter($deskServiceId) {
+            $businessDao = new BusinessDAO();
+            return $businessDao->getDeskServiceCounter($deskServiceId);
+        }
+        public function getDeskServiceBySportelistId($sportelistId) {
+            $filter = array("ID" => null, "BusinessID" => null,"Name"=>null, "SportelistID" => $sportelistId, "isActive" => 0);
+            $businessDao = new BusinessDAO();
+            $arr = $businessDao->getDeskService($filter);
+            $deskService = $arr[0];
+            return $deskService;
+        }
+    
+    
+    
+    public function saveBusiness(Business $business)
+    {
+        $businessDao = new BusinessDAO();
+        return $businessDao->saveBusiness($business);
+    }
+    public function saveBusinessEmployeeHeader(BusinessEmployeeHeader $businessEmployeeHeader) {
+        $businessDao = new BusinessDAO();
+        $businessDao->saveBusinessEmployeeHeader($businessEmployeeHeader);
+    }
+
+    public function updateBusiness(Business $business)
+    {
+        $businessDao = new BusinessDAO();
+        return $businessDao->updateBusiness($business, $business->getId());
+    }
+    public function deleteBusiness($businessId)
+    {
+        $businessDao = new BusinessDAO();
+        $businessDao->deleteBusiness($businessId);
+    }
+    public function deleteBusinessEmployeeHeader($businessId, $employeeId) {
+        $businessDao = new BusinessDAO();
+        $businessDao->deleteBusinessEmployeeHeader($businessId, $employeeId);
+    }
+
+
  
 
   
